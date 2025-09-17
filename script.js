7f296fb430dfc632164f1d6865a96c4ef13e8a1a
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-09-17 10:18:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+arr.sort((a,b)=>a-b)
+	let total=0
+while(arr.length>1){
+
+	let first=arr.shift()
+	let second=arr.shift()
+	total+=(first+second)
+	arr.push(first+second)
+	arr.sort((a,b)=>a-b)
+}  
+	return total
 }
 
 module.exports=mincost;
